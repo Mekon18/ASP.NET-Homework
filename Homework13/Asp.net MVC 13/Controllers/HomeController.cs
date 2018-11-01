b65d1c2adb0b574800383b36c0986cd7deac6e4c
Mekon18 <maksim.grummo@mail.ru>
2018-11-01 16:15:20 +0300

    HomeWork13

---
+++ Homework13/Asp.net MVC 13/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Asp.net_MVC_13.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.net_MVC_13.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Calc c = Calc.Create();
+
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calc calc)
+        {
+
+            double result = 0;
+            switch (calc.Operation)
+            {
+                case "+": result = calc.Arg1 + calc.Arg2; break;
+                case "-": result = calc.Arg1 - calc.Arg2; break;
+                case "*": result = calc.Arg1 * calc.Arg2; break;
+
+            }
+            if (calc.Result == result)
+                ViewBag.Message = "Correct";
+            else
+                ViewBag.Message = "Wrong";
+            Calc c = Calc.Create();
+            ModelState.Clear();
+            return View(c);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Homework13/Asp.net MVC 13/Models/Calc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.net_MVC_13.Models
+{
+    public class Calc
+    {
+        public double Arg1 { get; set; }
+        public double Arg2 { get; set; }
+        public string Operation { get; set; }
+        public double Result { get; set; }
+
+        public static Calc Create()
+        {
+            Calc c = new Calc();
+            Random rnd = new Random();
+            c.Arg1 = rnd.Next(1, 10);
+            c.Arg2 = rnd.Next(1, 10);
+            switch (rnd.Next(1, 3))
+            {
+                case 1: c.Operation = "+"; break;
+                case 2: c.Operation = "-"; break;
+                case 3: c.Operation = "*"; break;
+            }
+            return c;
+        }
+        public override string ToString()
+        {
+            return $"{Arg1} {Operation} {Arg2} = {Result}";
+        }
+    }
+}
